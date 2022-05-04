751cc757cb64ff38245723ece165bd79c01a3c75
Cookingwebs <info@cookingwebs.es>
2022-05-04 13:45:38 +0200

    Edase v1.2 - PRO - Landings nuevas aula abierta

--- resources/views/lanzamiento/aula-abierta-contable.blade.php
+++ resources/views/lanzamiento/aula-abierta-contable.blade.php
@@ -23,6 +23,9 @@
             <div class="ratio ratio-16x9">
                 <iframe src="https://player.vimeo.com/video/705333066?h=4e665f8990&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="fullscreen; picture-in-picture" allowfullscreen title="GO_Directo_Contabilida.mp4"></iframe><script src="https://player.vimeo.com/api/player.js"></script>
               </div>
+              <div class="--cta">
+                    <a href="{{route('aula-abierta-documentos')}}" target="_blank" rel="noopener noreferrer">DOCUMENTOS</a>
+              </div>
         </div>
     </div>
 
